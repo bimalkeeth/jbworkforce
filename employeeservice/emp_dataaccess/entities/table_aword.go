55e0283ad609f3251b69184572438dcb7c873788
vtel.mobicom <bimalkeeth@gmail.com>
2018-11-01 05:34:06 +1100

    Entities defined for GORM

--- employeeservice/emp_dataaccess/entities/table_aword.go
+++ employeeservice/emp_dataaccess/entities/table_aword.go
@@ -1,1 +1,13 @@
 package entities
+
+import "github.com/jinzhu/gorm"
+
+type TableAward struct {
+	gorm.Model
+	AwardName string `gorm:"column:awardname;not_null"`
+	AwardAbbr string `gorm:"column:awardabbr;not_null"`
+}
+
+func (c TableAward) TableName() string {
+	return "table_award"
+}
