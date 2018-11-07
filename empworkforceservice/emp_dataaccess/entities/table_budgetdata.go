bddcda6ff42a256d67bcad82351a545d045e74ab
Bimal Kaluarachchi <bkaluarachchi@assetic.com>
2018-11-07 22:41:57 +1100

    implemet master service

--- empworkforceservice/emp_dataaccess/entities/table_budgetdata.go
+++ empworkforceservice/emp_dataaccess/entities/table_budgetdata.go
@@ -9,45 +9,49 @@ type TableBudgetData struct {
 	StatusId              uint    `gorm:"column:statusid"`
 	ProfileStatus         string  `gorm:"column:profilestatus"`
 	HospitalId            uint    `gorm:"column:hospitalid"`
-	HospitalAbbr          string  `gorm:"column:description"`
-	CampusId              uint    `gorm:"column:description"`
-	CampusName            string  `gorm:"column:description"`
-	DivisionId            uint    `gorm:"column:description"`
-	DivisionName          string  `gorm:"column:description"`
-	DepartmentId          uint    `gorm:"column:description"`
-	DepartmentName        string  `gorm:"column:description"`
-	RosterUnitId          int     `gorm:"column:description"`
-	RosterUnitName        string  `gorm:"column:description"`
-	SecondaryCostCentreId uint    `gorm:"column:description"`
-	CostCentreIds         string  `gorm:"column:description"`
-	CostCentreNames       string  `gorm:"column:description"`
-	HealthCategoryId      uint    `gorm:"column:description"`
-	HealthCategoryAbbr    string  `gorm:"column:description"`
-	ClinicalStreamId      uint    `gorm:"column:description"`
-	ClinicalStreamAbbr    string  `gorm:"column:description"`
-	HealthGroupId         uint    `gorm:"column:description"`
-	HealthGroupName       string  `gorm:"column:description"`
-	GradeCodeId           uint    `gorm:"column:description"`
-	GradeCodeAbbr         string  `gorm:"column:description"`
-	GradeProfessionId     uint    `gorm:"column:description"`
-	GradeProfessionName   string  `gorm:"column:description"`
-	AllowanceSkillMixId   uint    `gorm:"column:description"`
-	SkillTypeId           uint    `gorm:"column:description"`
-	SkillTypeAbbr         string  `gorm:"column:description"`
-	StaffTypeId           uint    `gorm:"column:description"`
-	StaffTypeName         string  `gorm:"column:description"`
-	AllowanceId           uint    `gorm:"column:description"`
-	AllowanceAbbr         string  `gorm:"column:description"`
-	AllowanceGroupName    string  `gorm:"column:description"`
-	Budget                float64 `gorm:"column:description"`
-	Eft                   float64 `gorm:"column:description"`
-	BedHour               int     `gorm:"column:description"`
-	RosterEft             float64 `gorm:"column:description"`
-	BudgetBaseEft         float64 `gorm:"column:description"`
-	AnnualLeaveEft        float64 `gorm:"column:description"`
-	ADOEft                float64 `gorm:"column:description"`
-	SickEft               float64 `gorm:"column:description"`
-	PGradStudyEft         float64 `gorm:"column:description"`
-	ExtraBudgetEft        float64 `gorm:"column:description"`
-	BudgetEft             float64 `gorm:"column:description"`
+	HospitalAbbr          string  `gorm:"column:hospitalabbr"`
+	CampusId              uint    `gorm:"column:campusid"`
+	CampusName            string  `gorm:"column:campusname"`
+	DivisionId            uint    `gorm:"column:divisionid"`
+	DivisionName          string  `gorm:"column:divisionname"`
+	DepartmentId          uint    `gorm:"column:departmentid"`
+	DepartmentName        string  `gorm:"column:departmentname"`
+	RosterUnitId          int     `gorm:"column:rosterunitid"`
+	RosterUnitName        string  `gorm:"column:rosterunitname"`
+	SecondaryCostCentreId uint    `gorm:"column:secondarycostcentreid"`
+	CostCentreIds         string  `gorm:"column:costcentreids"`
+	CostCentreNames       string  `gorm:"column:costcentrenames"`
+	HealthCategoryId      uint    `gorm:"column:healthcategoryid"`
+	HealthCategoryAbbr    string  `gorm:"column:healthcategoryabbr"`
+	ClinicalStreamId      uint    `gorm:"column:clinicalstreamid"`
+	ClinicalStreamAbbr    string  `gorm:"column:clinicalstreamabbr"`
+	HealthGroupId         uint    `gorm:"column:healthgroupid"`
+	HealthGroupName       string  `gorm:"column:healthgroupname"`
+	GradeCodeId           uint    `gorm:"column:gradecodeid"`
+	GradeCodeAbbr         string  `gorm:"column:gradecodeabbr"`
+	GradeProfessionId     uint    `gorm:"column:gradeprofessionid"`
+	GradeProfessionName   string  `gorm:"column:gradeprofessionname"`
+	AllowanceSkillMixId   uint    `gorm:"column:allowanceskillmixId"`
+	SkillTypeId           uint    `gorm:"column:skilltypeid"`
+	SkillTypeAbbr         string  `gorm:"column:skilltypeabbr"`
+	StaffTypeId           uint    `gorm:"column:stafftypeid"`
+	StaffTypeName         string  `gorm:"column:stafftypename"`
+	AllowanceId           uint    `gorm:"column:allowanceid"`
+	AllowanceAbbr         string  `gorm:"column:allowanceabbr"`
+	AllowanceGroupName    string  `gorm:"column:allowancegroupname"`
+	Budget                float64 `gorm:"column:budget;default:0.00"`
+	Eft                   float64 `gorm:"column:eft;default:0.00"`
+	BedHour               int     `gorm:"column:bedhour;default:0"`
+	RosterEft             float64 `gorm:"column:rostereft;default:0.00"`
+	BudgetBaseEft         float64 `gorm:"column:budgetbaseeft;default:0.00"`
+	AnnualLeaveEft        float64 `gorm:"column:annualleaveeft;default:0.00"`
+	ADOEft                float64 `gorm:"column:adoeft;default:0.00"`
+	SickEft               float64 `gorm:"column:sickeft;default:0.00"`
+	PGradStudyEft         float64 `gorm:"column:pgradstudyeft;default:0.00"`
+	ExtraBudgetEft        float64 `gorm:"column:extrabudgeteft;default:0.00"`
+	BudgetEft             float64 `gorm:"column:budgeteft;default:0.00"`
+}
+
+func (c TableBudgetData) TableName() string {
+	return "table_budgetdata"
 }
