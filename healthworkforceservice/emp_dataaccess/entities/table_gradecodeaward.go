abc86a9b6dfbb80602a48f75a9da9e009a0673ed
bimalkeeth@gmail.com <Scala@1234>
2018-11-11 21:02:58 +1100

    Qualification Table

--- healthworkforceservice/emp_dataaccess/entities/table_gradecodeaward.go
+++ healthworkforceservice/emp_dataaccess/entities/table_gradecodeaward.go
@@ -4,4 +4,12 @@ import "github.com/jinzhu/gorm"
 
 type TableGradeCodeAward struct {
 	gorm.Model
+	GradeCodeId  uint    `gorm:"column:gradecodeid;not_null"`
+	GradeAwardId uint    `gorm:"column:gradeawardid;not_null"`
+	HourlyRate   float64 `gorm:"column:hourlyrate;not_null"`
+	UpdateUserId uint    `gorm:"column:updateuserid;not_null"`
+}
+
+func (c TableGradeCodeAward) TableName() string {
+	return "table_gradecodeaward"
 }
