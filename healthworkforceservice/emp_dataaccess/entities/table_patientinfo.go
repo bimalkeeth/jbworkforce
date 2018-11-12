91a38041c0a7d455e07a16b8edfc81cf7698574a
bimalkeeth@gmail.com <Scala@1234>
2018-11-13 01:50:50 +1100

    Qualification Table

---
+++ healthworkforceservice/emp_dataaccess/entities/table_patientinfo.go
@@ -0,0 +1,7 @@
+package entities
+
+import "github.com/jinzhu/gorm"
+
+type TablePatientInfo struct {
+	gorm.Model
+}
--- masterdataservice/master_dataaccess/entities/table_hospital.go
+++ masterdataservice/master_dataaccess/entities/table_hospital.go
@@ -4,4 +4,19 @@ import "github.com/jinzhu/gorm"
 
 type TableHospital struct {
 	gorm.Model
+	OrganisationId    uint              `gorm:"column:organisationid;not_null"`
+	HospitalName      string            `gorm:"column:hospitalname;not_null"`
+	HospitalAbbr      string            `gorm:"column:hospitalabbr;not_null"`
+	HasWorkforce      bool              `gorm:"column:hasworkforce;not_null"`
+	HasEftProfiler    bool              `gorm:"column:haseftprofiler;not_null"`
+	HasYearlyPlanner  bool              `gorm:"column:hasyearlyplanner;not_null"`
+	HasAllocation     bool              `gorm:"column:hasallocation;not_null"`
+	IsActive          bool              `gorm:"column:isactive;not_null"`
+	Description       string            `gorm:"column:description;not_null"`
+	HospitalLogo      string            `gorm:"column:hospitallogo;not_null"`
+	TableOrganisation TableOrganisation `gorm:"foreignkey:organisationid"`
+}
+
+func (c TableHospital) TableName() string {
+	return "table_hospital"
 }
