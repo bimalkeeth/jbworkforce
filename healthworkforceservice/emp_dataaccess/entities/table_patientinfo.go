6cc64a60768a1d1509a10c27ef4032ed2e3209a8
bimalkeeth@gmail.com <Scala@1234>
2018-11-13 04:52:43 +1100

    Patient Table

--- healthworkforceservice/emp_dataaccess/entities/table_patientinfo.go
+++ healthworkforceservice/emp_dataaccess/entities/table_patientinfo.go
@@ -4,4 +4,24 @@ import "github.com/jinzhu/gorm"
 
 type TablePatientInfo struct {
 	gorm.Model
+	URNumber        string `gorm:"column:urnumber"`
+	FirstName       string `gorm:"column:firstname"`
+	LastName        string `gorm:"column:lastname"`
+	IsAcute         bool   `gorm:"column:isacute"`
+	IsAggression    bool   `gorm:"column:isaggression"`
+	IsSuicidal      bool   `gorm:"column:issuicidal"`
+	IsWandering     bool   `gorm:"column:iswandering"`
+	IsConfusion     bool   `gorm:"column:isconfusion"`
+	IsSelfHarm      bool   `gorm:"column:isselfharm"`
+	IsDisInhibited  bool   `gorm:"column:isdisinhibited"`
+	IsFalls         bool   `gorm:"column:isfalls"`
+	IsHomicidal     bool   `gorm:"column:ishomicidal"`
+	IsMedicalAcuity bool   `gorm:"column:ismedicalacuity"`
+	IsOther         bool   `gorm:"column:isother"`
+	Comments        string `gorm:"column:comments"`
+	IsActive        bool   `gorm:"column:isactive"`
+}
+
+func (c TablePatientInfo) TableName() string {
+	return "table_patientinfo"
 }
