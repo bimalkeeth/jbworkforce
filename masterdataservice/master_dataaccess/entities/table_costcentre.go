2b504f80d763201067d977838fa0d5a610cd2d95
Bimalkeeth <bkaluarachchi@assetic.com>
2018-12-06 14:41:59 +1100

    Setup relations

--- masterdataservice/master_dataaccess/entities/table_costcentre.go
+++ masterdataservice/master_dataaccess/entities/table_costcentre.go
@@ -4,17 +4,18 @@ import "github.com/jinzhu/gorm"
 
 type TableCostCentre struct {
 	gorm.Model
-	HospitalId     uint    `gorm:"column:hospitalid;not_null"`
-	CostCentreName string  `gorm:"column:costcentrename;not_null"`
-	CostCentreAbbr string  `gorm:"column:costcentreabbr;not_null"`
-	CostCentreType string  `gorm:"column:costcentretype"`
-	Description    string  `gorm:"column:description"`
-	IsActive       bool    `gorm:"column:isactive;default:false"`
-	InPayroll      bool    `gorm:"column:inpayroll;default:false"`
-	RosterEft      float64 `gorm:"column:rostereft;default:0.00"`
-	BudgetEft      float64 `gorm:"column:budgeteft;default:0.00"`
-	BudgetBaseEft  float64 `gorm:"column:budgetbaseeft;default:0.00"`
-	BudgetDollar   float64 `gorm:"column:budgetdollar;default:0.00"`
+	HospitalId     uint          `gorm:"column:hospitalid;not_null"`
+	CostCentreName string        `gorm:"column:costcentrename;not_null"`
+	CostCentreAbbr string        `gorm:"column:costcentreabbr;not_null"`
+	CostCentreType string        `gorm:"column:costcentretype"`
+	Description    string        `gorm:"column:description"`
+	IsActive       bool          `gorm:"column:isactive;default:false"`
+	InPayroll      bool          `gorm:"column:inpayroll;default:false"`
+	RosterEft      float64       `gorm:"column:rostereft;default:0.00"`
+	BudgetEft      float64       `gorm:"column:budgeteft;default:0.00"`
+	BudgetBaseEft  float64       `gorm:"column:budgetbaseeft;default:0.00"`
+	BudgetDollar   float64       `gorm:"column:budgetdollar;default:0.00"`
+	TableHospital  TableHospital `gorm:"foreignkey:hospitalid"`
 }
 
 func (c TableCostCentre) TableName() string {
